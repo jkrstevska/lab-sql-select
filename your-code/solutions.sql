54d1104c524c273f3cb58669faaa09db25eb6358
jkrstevska <jovana.krstevska99@gmail.com>
2026-09-24 17:29:52 +0200

    W2D4_Lab1

---
+++ your-code/solutions.sql
@@ -0,0 +1,78 @@
+/*
+	Challenge 1
+*/
+
+SELECT
+	a.au_id AS 'AUTHOR ID',
+	a.au_lname AS 'LAST NAME',
+	a.au_fname AS 'FIRST NAME',
+	t.title AS 'TITLE',
+	p.pub_name AS 'PUBLISHER'
+FROM authors a
+JOIN titleauthor ta 
+	ON a.au_id = ta.au_id
+JOIN titles t
+	ON ta.title_id = t.title_id
+LEFT JOIN publishers p
+	ON t.pub_id = p.pub_id;
+
+/*
+	Challenge 2
+*/
+
+SELECT
+	a.au_id AS 'AUTHOR ID',
+	a.au_lname AS 'LAST NAME',
+	a.au_fname AS 'FIRST NAME',
+	t.title AS 'TITLE',
+	p.pub_name AS 'PUBLISHER',
+	COUNT(t.title_id) AS 'TITLE COUNT'
+FROM authors a
+JOIN titleauthor ta 
+	ON a.au_id = ta.au_id
+JOIN titles t
+	ON ta.title_id = t.title_id
+LEFT JOIN publishers p
+	ON t.pub_id = p.pub_id
+GROUP BY a.au_id, a.au_lname, a.au_fname, p.pub_name
+ORDER BY COUNT(t.title_id) DESC
+
+/*
+Challenge 3 - Best Selling Authors
+*/
+
+SELECT
+	a.au_id AS 'AUTHOR ID',
+	a.au_lname AS 'LAST NAME',
+	a.au_fname AS 'FIRST NAME',
+	SUM(s.qty) AS 'TOTAL'
+-- 	COUNT(t.title_id) AS 'TOTAL'
+FROM authors a
+JOIN titleauthor ta 
+	ON a.au_id = ta.au_id
+JOIN titles t
+	ON ta.title_id = t.title_id
+LEFT JOIN sales s
+	ON t.title_id = s.title_id
+GROUP BY a.au_id, a.au_lname, a.au_fname
+ORDER BY SUM(s.qty) DESC
+LIMIT 3
+
+/*
+Challenge 4 - Best Selling Authors Ranking
+*/
+
+SELECT
+	a.au_id AS 'AUTHOR ID',
+	a.au_lname AS 'LAST NAME',
+	a.au_fname AS 'FIRST NAME',
+	COALESCE(SUM(s.qty), 0) AS 'TOTAL'
+FROM authors a
+LEFT JOIN titleauthor ta 
+	ON a.au_id = ta.au_id
+LEFT JOIN titles t
+	ON ta.title_id = t.title_id
+LEFT JOIN sales s
+	ON t.title_id = s.title_id
+GROUP BY a.au_id, a.au_lname, a.au_fname
+ORDER BY SUM(s.qty) DESC
